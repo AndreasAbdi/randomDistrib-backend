5beb8083a8ba9d104dfc067c5ae70362804e6281
AndreasAbdi <andreasabdi@yahoo.ca>
2016-12-07 14:26:20 -0800

    Started some of the services for the client

--- src/socket/client.ts
+++ src/socket/client.ts
@@ -1,40 +1,61 @@
 export default function setupSocket(socket: SocketIO.Socket, io: SocketIO.Server): void {
+    addEventListeners(socket, io);
+}
+
+function addEventListeners(socket: SocketIO.Socket, io: SocketIO.Server): void {
     socket.on('connect', connect(io));
     socket.on('disconnect', disconnect(io));
     socket.on('add-slice', addSlice(io));
     socket.on('remove-slice', removeSlice(io));
     socket.on('get-distribution', getDistribution(io));
     socket.on('decide', decide(io));
+    socket.on('list', list(socket, io));
+    socket.on('chat-message', chatMessage);
 }
 
+function chatMessage(message: any): void {
+    console.log(message);
+}
+
+function list(socket: SocketIO.Socket, io: SocketIO.Server): () => any {
+    //TODO
+    return () => {
+        let data = [
+            { name: `LOL`, probability: 50 },
+            { name: `OW`, probability: 50 }
+        ];
+
+        socket.emit('list', data);
+    };
+}
 function connect(io: SocketIO.Server): () => void {
     //TODO
-    return null;
+    return () => { };
 }
 
 function disconnect(io: SocketIO.Server): () => void {
-    //TODO
-    return null;
+    // TODO
+    return () => { };
 }
 
 function addSlice(io: SocketIO.Server): () => void {
-    //TODO
-    return null;
+    // TODO
+    return () => { };
 }
 
 function removeSlice(io: SocketIO.Server): () => void {
-    //TODO
-    return null;
+    // TODO
+    return () => { };
 
 }
 
 function getDistribution(io: SocketIO.Server): () => void {
-    //TODO
-    return null;
+    // TODO
+    return () => { };
 
 }
 
 function decide(io: SocketIO.Server): () => void {
-    //TODO
-    return null;
+    // TODO
+    return () => { };
 }
