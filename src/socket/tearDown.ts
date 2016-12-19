065559e000a2f2075b3e1b049c4aaa61ed1eef5e
AndreasAbdi <andreasabdi@yahoo.ca>
2016-12-18 19:31:35 -0800

    update all users when teardown occurs

--- src/socket/tearDown.ts
+++ src/socket/tearDown.ts
@@ -1,5 +1,6 @@
-import {removeName} from '../services/client-service';
+import { getClients, removeName } from '../services/client-service';
 
-export default function tearDown (socket: SocketIO.Socket, io: SocketIO.Server) {
+export default function tearDown(socket: SocketIO.Socket, io: SocketIO.Server) {
   removeName(socket, io);
+  getClients(socket, io);
 }
